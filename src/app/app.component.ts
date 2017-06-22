7c3aed4bc81b4235be759ab589869e6bb24f4733
olegbezk <oleg>
2017-06-22 14:38:38 +0300

    add number increment function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+    onIntervalFired(data:number) {
+        console.log(data);
+    }
 }
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, EventEmitter, Output} from '@angular/core';
 
 @Component({
-  selector: 'app-game',
-  templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+    selector: 'app-game-control',
+    templateUrl: './game.component.html',
+    styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
+    @Output() intervalFired = new EventEmitter<number>();
+    interval;
+    lastNumber = 0;
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    onStart() {
+        this.interval = setInterval(() => {
+            this.intervalFired.emit(this.lastNumber + 1);
+            this.lastNumber++;
+        }, 1000);
+    }
+
+    onPause() {
+    }
 
 }
